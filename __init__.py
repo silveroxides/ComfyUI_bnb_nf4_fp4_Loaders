7a194740d60fe2e52fb31bb6b38918088d1f2278
silveroxides <ishimarukaito@gmail.com>
2025-01-20 09:28:30 +0100

    Add preliminary WIP text encoder loaders

--- __init__.py
+++ __init__.py
@@ -174,6 +174,8 @@ class OPS(comfy.ops.manual_cast):
 
 
 class CheckpointLoaderNF4:
+    NodeId = 'CheckpointLoaderNF4'
+    NodeName = 'Load FP4 or NF4 Quantized Checkpoint Model'
     @classmethod
     def INPUT_TYPES(s):
         return {"required": { "ckpt_name": (folder_paths.get_filename_list("checkpoints"), ),
@@ -189,6 +191,8 @@ class CheckpointLoaderNF4:
         return out[:3]
 
 class UNETLoaderNF4:
+    NodeId = 'UNETLoaderNF4'
+    NodeName = 'Load FP4 or NF4 Quantized Diffusion or UNET Model'
     @classmethod
     def INPUT_TYPES(s):
         return {"required": { "unet_name": (folder_paths.get_filename_list("diffusion_models"), ),
@@ -203,12 +207,87 @@ class UNETLoaderNF4:
         model = comfy.sd.load_diffusion_model(unet_path, model_options={"custom_operations": OPS})
         return (model,)
 
-NODE_CLASS_MAPPINGS = {
-    "CheckpointLoaderNF4": CheckpointLoaderNF4,
-    "UNETLoaderNF4": UNETLoaderNF4,
-}
+class CLIPLoaderNF4:
+    # WIP
+    NodeId = 'CLIPLoaderNF4'
+    NodeName = 'Load FP4 or NF4 Quantized Text Encoder'
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": { "clip_name": (folder_paths.get_filename_list("text_encoders"), ),
+                              "type": (["stable_diffusion", "stable_cascade", "sd3", "stable_audio", "mochi", "ltxv", "pixart", "cosmos"], ),
+                              },}
+    RETURN_TYPES = ("CLIP",)
+    FUNCTION = "load_clip"
 
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "CheckpointLoaderNF4": "Load NF4 Flux Checkpoint",
-    "UNETLoaderNF4": "Load NF4 Flux UNET",
-}
+    CATEGORY = "advanced/loaders"
+
+    def load_clip(self, clip_name, type):
+        if type == "stable_cascade":
+            clip_type = comfy.sd.CLIPType.STABLE_CASCADE
+        elif type == "sd3":
+            clip_type = comfy.sd.CLIPType.SD3
+        elif type == "stable_audio":
+            clip_type = comfy.sd.CLIPType.STABLE_AUDIO
+        elif type == "mochi":
+            clip_type = comfy.sd.CLIPType.MOCHI
+        elif type == "ltxv":
+            clip_type = comfy.sd.CLIPType.LTXV
+        elif type == "pixart":
+            clip_type = comfy.sd.CLIPType.PIXART
+        else:
+            clip_type = comfy.sd.CLIPType.STABLE_DIFFUSION
+
+        clip_path = folder_paths.get_full_path("clip", clip_name)
+        clip = comfy.sd.load_clip(ckpt_paths=[clip_path], embedding_directory=folder_paths.get_folder_paths("embeddings"), clip_type=clip_type, model_options={"custom_operations": OPS})
+        return (clip,)
+
+class DualCLIPLoaderNF4:
+    # WIP
+    NodeId = 'DualCLIPLoaderNF4'
+    NodeName = 'Load FP4 or NF4 Quantized Text Encoders'
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": { "clip_name1": (folder_paths.get_filename_list("text_encoders"), ),
+                              "clip_name2": (folder_paths.get_filename_list("text_encoders"), ),
+                              "type": (["sdxl", "sd3", "flux", "hunyuan_video", "custom_flux", "fluxmod"], ),
+                              },}
+    RETURN_TYPES = ("CLIP",)
+    FUNCTION = "load_clip"
+
+    CATEGORY = "advanced/loaders"
+
+    def load_clip(self, clip_name1, clip_name2, type):
+        clip_path1 = folder_paths.get_full_path_or_raise("text_encoders", clip_name1)
+        clip_path2 = folder_paths.get_full_path_or_raise("text_encoders", clip_name2)
+        if type == "sdxl":
+            clip_type = comfy.sd.CLIPType.STABLE_DIFFUSION
+        elif type == "sd3":
+            clip_type = comfy.sd.CLIPType.SD3
+        elif type == "flux":
+            clip_type = comfy.sd.CLIPType.FLUX
+        elif type == "hunyuan_video":
+            clip_type = comfy.sd.CLIPType.HUNYUAN_VIDEO
+        elif type == "custom_flux":
+            clip_type = comfy.sd.CLIPType.FLUXC
+        elif type == "fluxmod":
+            clip_type = comfy.sd.CLIPType.FLUXMOD
+
+        clip = comfy.sd.load_clip(ckpt_paths=[clip_path1, clip_path2], embedding_directory=folder_paths.get_folder_paths("embeddings"), clip_type=clip_type, model_options={"custom_operations": OPS})
+        return (clip,)
+
+node_list = [
+    # Checkpoint model loaders
+    CheckpointLoaderNF4,
+    # Diffusion model loaders
+    UNETLoaderNF4,
+    # Text encoder model loaders
+    # WIP CLIPLoaderNF4,
+    # WIP DualCLIPLoaderNF4,
+]
+
+NODE_CLASS_MAPPINGS = {}
+NODE_DISPLAY_NAME_MAPPINGS = {}
+
+for node in node_list:
+    NODE_CLASS_MAPPINGS[node.NodeId] = node
+    NODE_DISPLAY_NAME_MAPPINGS[node.NodeId] = node.NodeName
